8e2f5b902876a99bf01fa4b7a69a69ed30a4c9a1
Ignacio Elizardo Millanao Moya <60947522+IgnacioM98@users.noreply.github.com>
2020-07-24 09:10:07 -0400

    Minimal changes

--- camara-live.py
+++ camara-live.py
@@ -17,7 +17,7 @@ nombres_encoding = [
 cam = cv2.VideoCapture(0)
 
 
-contadorDesconocidos = 0
+con = 0 # Cuenta Frames
 nombres_rostros = []
 
 while True:
@@ -35,7 +35,7 @@ while True:
         encodings_rostros = face_recognition.face_encodings(img_rgb, ubi_rostro)
 
         
-        color = (0, 255, 0) # Verde por defecto
+        color = (0, 255, 0) # Verde por defecto / Bordes rostro
         
         nombres_rostros = []
         for encoding in encodings_rostros:
@@ -45,25 +45,27 @@ while True:
             if True in coincidencias:
                 nom = nombres_encoding[coincidencias.index(True)]
                 color = (0, 255, 0)
-                contadorDesconocidos = 0
+                con = 0
                     
             else:
                 nom ="DESCONOCIDO"
                 color = (0, 0, 255)
-                contadorDesconocidos+=1
+                con += 1
 
             nombres_rostros.append(nom)
    
 
     for ((top, right, bottom, left), nom) in zip(ubi_rostro, nombres_rostros):
-        # Alerta
-        if contadorDesconocidos>5:
-            subprocess.run('python WhatsappMessage.py', shell=True)
-            messagebox.showinfo('ALERTA', 'No se ha podido detectar un usuario registrado')
-            contadorDesconocidos = 0
+
+        # Alerta - Puede ser timer o contador de Frames
+        if con>5:
+            messagebox.showwarning(message="Unknown user detected", title="Alert")
             cv2.imwrite('desconocido.jpg', img)
+            subprocess.run('python WhatsappMessage.py', shell=True)
+            con = 0
         
-        #Re-escalamiento
+        
+        # Re-escalamiento
         top = int(top * 5)
         right = int(right * 5)
         bottom = int(bottom * 5)
@@ -73,9 +75,9 @@ while True:
         cv2.rectangle(img, (left, top), (right, bottom), color, 2)
         cv2.putText(img, nom, (left, bottom + 20), cv2.FONT_HERSHEY_COMPLEX, 0.6, (255,255,255), 1)
 
-    cv2.imshow('Live', img)   
+    cv2.imshow('Live', img) # Imagen
 
     if cv2.waitKey(1) == 27:  
-        break  # Esc
+        break  # Esc - Salir
 
 cv2.destroyAllWindows()
