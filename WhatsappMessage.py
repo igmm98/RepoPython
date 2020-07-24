e70880665922e67417762584d54df23e6852aeee
igmm98 <60943230+igmm98@users.noreply.github.com>
2020-07-24 05:05:37 -0400

    safe

--- WhatsappMessage.py
+++ WhatsappMessage.py
@@ -3,14 +3,14 @@ from twilio.rest import Client
 
 #
 # ES NECESARIO REGISTRO EN WEB DE TWILIO
-account_sid = 'ACccfed01325e0fe30539ab253e169dfa8' 
-auth_token = 'cecbc2d51738c60ee2fd2bed4d458fb6' 
+account_sid = '' 
+auth_token = '' 
 client = Client(account_sid, auth_token) 
  
 message = client.messages.create( 
                               from_='whatsapp:+14155238886',  
                               body='*ALERTA \nShielder*',      
-                              to='whatsapp:+56945659268' 
+                              to='whatsapp:+' 
                           ) 
  
 print(message.sid)
